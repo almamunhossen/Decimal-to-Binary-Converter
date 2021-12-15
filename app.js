66599210239fa3fd2318215ecec245ea7921d76b
almamunhossen <almamunhossen@gmail.com>
2021-12-15 21:49:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+let submitButton = document.querySelector("#submit-button");
+let result = document.querySelector("#results");
+
+function binaryContent(x) {
+  x.preventDefault();
+  let inputNumber = document.querySelector("#input-number").value;
+  if (inputNumber === "") {
+    alert("Please enter a number");
+  } else if (inputNumber < 0) {
+    alert("Please enter a positive number");
+  } else {
+    // result.style.visibility = "visible";
+    result.style.display = "block";
+  }
+
+  // Convolver to Binary
+  let binary = Number(inputNumber).toString(2);
+  result.innerHTML = binary;
+}
+submitButton.addEventListener("click", binaryContent);
